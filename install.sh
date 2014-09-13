81e46f4fac5d43799e9db9dd7bef512994300a75
Mateus Gomes <mateusg.18@gmail.com>
2014-09-13 16:48:01 -0300

    Change install script to create symbolic links

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!bin/sh
 
-cp files/gemrc ~/.gemrc
-cp files/gitconfig ~/.gitconfig
-cp files/irbrc ~/.irbrc
-cp files/pryrc ~/.pryrc
-cp files/rvmrc ~/.rvmrc
-cp files/vimrc ~/.vimrc
-cp files/zshrc ~/.zshrc
+ln -sf $(pwd)/files/gemrc ~/.gemrc
+ln -sf $(pwd)/files/gitconfig ~/.gitconfig
+ln -sf $(pwd)/files/irbrc ~/.irbrc
+ln -sf $(pwd)/files/pryrc ~/.pryrc
+ln -sf $(pwd)/files/rvmrc ~/.rvmrc
+ln -sf $(pwd)/files/vimrc ~/.vimrc
+ln -sf $(pwd)/files/zshrc ~/.zshrc
 
 mkdir -p ~/.oh-my-zsh/themes/
-cp files/.oh-my-zsh/themes/mateusg.zsh-theme ~/.oh-my-zsh/themes/mateusg.zsh-theme
+ln -sf $(pwd)/files/.oh-my-zsh/themes/mateusg.zsh-theme ~/.oh-my-zsh/themes/mateusg.zsh-theme
